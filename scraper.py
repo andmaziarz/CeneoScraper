4365a025f819dea90a4c86f84bdcd6d53a866b14
andmaziarz <a.maziarz9@outlook.com>
2022-04-09 13:17:09 +0200

    2022-04-09 AM

--- scraper.py
+++ scraper.py
@@ -1,7 +1,19 @@
 from urllib import response
 import requests
+from turtle import st
+from bs4 import BeautifulSoup
+
 
 url ="https://www.ceneo.pl/101052360#tab=reviews"
 response = requests.get(url)
 
-print(response.status_code)
+page = BeautifulSoup(response.text, "html.parser")
+
+opinions = page.select("div.js_product-review")
+opinion = opinions.pop(0)
+opinion_id = opinion["data-entry-id"]
+author = opinion.select_one("span.user-post__author-name").get_text().strip()
+recommendation = opinion.select_one("span.user-post__author-recomendation > em").get_text().strip()
+stars = opinion.select_one("span.user-post__score-count").get_text().strip()
+
+print(author)
