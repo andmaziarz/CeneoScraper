eb29aa8afa504a964f46e4ce016ac36158a7dfcc
andmaziarz <a.maziarz9@outlook.com>
2022-04-28 14:39:53 +0200

    2022-04-28 AM

--- scraper.py
+++ scraper.py
@@ -16,6 +16,10 @@ author = opinion.select_one("span.user-post__author-name").get_text().strip()
 recommendation = opinion.select_one("span.user-post__author-recomendation > em").get_text().strip()
 stars = opinion.select_one("span.user-post__score-count").get_text().strip()
 content = opinion.select_one("div.user-post__text").get_text().strip()
-
+useful = opinion.select_one("button.vote-yes > span").get_text().strip()
+useless = opinion.select_one("button.vote-no > span").get_text().strip()
+published = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
+purchased = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
+print(author, recommendation, stars, content, useful, useless, published, purchased, sep="\n")
 
 print(author)
